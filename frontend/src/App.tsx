eeda98509b31763ff4db51c1738b2804ba603f5e
Leo Belanger <belanger.leo01@gmail.com>
2022-10-05 09:51:15 -0400

    Basic form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 function App() {
   return (
     <ChakraProvider>
-      <HomePage /> 
+      <HomePage />
     </ChakraProvider>
   );
 }
--- frontend/src/components/EventCreationForm.tsx
+++ frontend/src/components/EventCreationForm.tsx
@@ -7,14 +7,32 @@ import {
   Box,
   FormControl,
   FormLabel,
-  FormErrorMessage,
   FormHelperText,
+  Grid,
+  GridItem,
   Input,
-} from '@chakra-ui/react'
-import { useForm, Controller } from "react-hook-form";
+  Textarea,
+} from '@chakra-ui/react';
+import { useForm, Controller } from 'react-hook-form';
+
+const endDateErrorMessage = (error: { [key: string]: any }) => {
+  switch (error.type) {
+    case "higherThanStartDate":
+      return "End date should be after start date.";
+    default:
+      return "Required.";
+  }
+}
 
 const EventCreationForm = () => {
-  const { control, register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    control,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm();
+
+  console.log({ errors });
 
   const onSubmit = (data: any) => console.log(data);
 
@@ -23,7 +41,7 @@ const EventCreationForm = () => {
       <AccordionItem>
         <h2>
           <AccordionButton>
-            <Box flex='1' textAlign='left'>
+            <Box flex='1' textAlign='left' pl={6}>
               Create Event
             </Box>
             <AccordionIcon />
@@ -31,25 +49,86 @@ const EventCreationForm = () => {
         </h2>
         <AccordionPanel pb={4}>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <Controller
-              name="name"
-              control={control}
-              rules={{ required: true }}
-              render={({ field }) => (
-                <FormControl>
-                  <FormLabel>Name</FormLabel>
-                  <Input {...field} />
-                  {errors.name && <FormHelperText>Something something error.</FormHelperText>}
-                </FormControl>
-              )}
-            />
-        
-            <Input type="submit" />
+            <Grid gap={6} pl={6} w='50%'>
+              <GridItem>
+                <Controller
+                  name='name'
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <FormControl>
+                      <FormLabel>Name</FormLabel>
+                      <Input {...field} />
+                      {errors.name && (
+                        <FormHelperText>Required.</FormHelperText>
+                      )}
+                    </FormControl>
+                  )}
+                />
+              </GridItem>
+
+              <GridItem>
+                <Controller
+                  name='description'
+                  control={control}
+                  render={({ field }) => (
+                    <FormControl>
+                      <FormLabel>Description</FormLabel>
+                      <Textarea {...field} />
+                    </FormControl>
+                  )}
+                />
+              </GridItem>
+
+              <GridItem>
+                <Controller
+                  name='startDate'
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <FormControl>
+                      <FormLabel>Start date</FormLabel>
+                      <Input {...field} type='datetime-local' />
+                      {errors.startDate && (
+                        <FormHelperText>Required.</FormHelperText>
+                      )}
+                    </FormControl>
+                  )}
+                />
+              </GridItem>
+
+              <GridItem>
+                <Controller
+                  name='endDate'
+                  control={control}
+                  rules={{ 
+                    required: true,
+                    validate: {
+                      higherThanStartDate: value => value > getValues().startDate,
+                    }
+                  }}
+                  render={({ field }) => (
+                    <FormControl>
+                      <FormLabel>End date</FormLabel>
+                      <Input {...field} type='datetime-local' />
+                      {errors.endDate && (
+                        <FormHelperText>{endDateErrorMessage(errors.endDate)}</FormHelperText>
+                      )}
+                    </FormControl>
+                  )}
+                />
+              </GridItem>
+
+              {/* //TODO: add timezone selector */}
+              <GridItem>
+                <Input type='submit' />
+              </GridItem>
+            </Grid>
           </form>
         </AccordionPanel>
       </AccordionItem>
     </Accordion>
-  )
-}
+  );
+};
 
 export default EventCreationForm;
